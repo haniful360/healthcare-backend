67588e21ea61b6c92d0eaf529ddd940ed8c1971b
Haniful Islam <haniful360@gmail.com>
2026-08-09 13:08:33 +0600

    project setup

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/v1/auth', AuthRoutes)
+// app.use('/api/v1/auth', AuthRoutes)
 
 // Basic route
 app.get('/', async (req: Request, res: Response) => {
@@ -33,7 +33,7 @@ app.get('/', async (req: Request, res: Response) => {
     })
 })
 
-app.use(globalErrorHandler)
+// app.use(globalErrorHandler)
 app.use(notFound)
 
 export default app
