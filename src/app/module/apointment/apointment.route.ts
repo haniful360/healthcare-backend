908d58716f859c291c25a941fd4bd2a0718b1666
Haniful Islam <hanifulislam160@gmail.com>
2026-08-23 00:13:25 +0600

    add appointment

---
+++ src/app/module/apointment/apointment.route.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { AppointmentController } from "./apointment.controller";
+
+
+const router = Router();
+
+router.post("/book-appointment", AppointmentController.bookAppointment);
+
+//book appointment callback url
+router.get(
+  "/book-appointment/payment/callback",
+  AppointmentController.bookAppointmentCallback,
+);
+
+export const AppointementRoutes = router;
